49ac6838cadf7784f27e52932e19c9528c599c89
suhas-doddabela <57515107+suhas-doddabela@users.noreply.github.com>
2019-11-11 07:38:07 -0500

    Update CelebrityIndustryContext.cs

--- Data/Rbc.Hologram.Data/Contexts/CelebrityIndustryContext.cs
+++ Data/Rbc.Hologram.Data/Contexts/CelebrityIndustryContext.cs
@@ -38,7 +38,8 @@ namespace Rbc.Hologram.Data.Contexts
             using (var c = new CelebrityHologramContext())
             {
                 c.CelebrityIndustry.Add(celebIndustry);
-                return c.SaveChanges();
+                c.SaveChanges();
+                return celebIndustry.CelebrityIndustryId;
             }
         }
 
